2b7409f64406b141c5d63e1eb34da353ba89d933
Fernando Rocha <fernandorff92@gmail.com>
2022-09-19 20:08:56 -0300

    finalizando

--- src/calculadora.js
+++ src/calculadora.js
@@ -1,5 +1,40 @@
 export const OPERACAO_INVALIDA = 'OPERACAO_INVALIDA'
 
 export const calculadora = (operacao, valores) => {
-  // código aqui
+  switch (operacao) {
+    case 'soma':
+      soma = valores[0]
+      for (let index = 1; index < valores.length; index++) {
+        soma += valores[i]
+        
+      }
+      return soma;
+    
+    case 'subtracao':
+      subtracao = valores[0]
+      for (let index = 1; index < valores.length; index++) {
+        subtracao -= valores[i]
+        
+      }
+      return subtracao;
+
+    case 'multiplicacao':
+      multiplicacao = valores[0]
+      for (let index = 1; index < valores.length; index++) {
+        multiplicacao *= valores[i]
+        
+      }
+      return multiplicacao;
+
+    case 'divisao':
+      divisao = valores[0]
+      for (let index = 1; index < valores.length; index++) {
+        divisao /= valores[i]
+        
+      }
+      return divisao;
+
+    default:
+      return OPERACAO_INVALIDA;
+  }
 }
